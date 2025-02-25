e700fec5d601792e8fca3df635ef42cb4a937000
sriram0328 <150550951+sriram0328@users.noreply.github.com>
2025-02-25 14:23:18 +0530

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import flet as ft
-
-def main(page: ft.Page):
-    page.title = "Image Recognition"
-    page.add(ft.Text("Hello, Flet!"))
-
-ft.app(target=main, view=ft.WEB_BROWSER)  # This ensures it runs as a web app
